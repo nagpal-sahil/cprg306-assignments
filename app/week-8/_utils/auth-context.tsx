97f02b9c0c67727d8112ebd3840288fc3abe022d
nagpal-sahil <sahil.nagpal@edu.sait.ca>
2026-03-12 15:29:27 -0600

    new-fix

--- app/week-8/_utils/auth-context.tsx
+++ app/week-8/_utils/auth-context.tsx
@@ -9,7 +9,7 @@ import {
 } from "firebase/auth";
 import { auth } from "./firebase";
 
-const AuthContext = createContext();
+const AuthContext = createContext<{ user: any; gitHubSignIn: () => any; firebaseSignOut: () => any } | undefined>(undefined);
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
